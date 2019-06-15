f114c0773e53011d3dcd126ce088ff75e2ae7fe3
chandrakant <chandrakant7892@gmail.com>
2019-06-15 16:15:32 +0530

    Classes

--- object_oriented.py
+++ object_oriented.py
@@ -1,4 +1,6 @@
 class Computer:
+    manufactur = 'Sony'
+
     def __init__(self, cpu, ram):
         print("In Init")
         self.cpu = cpu
@@ -7,6 +9,11 @@ class Computer:
     def config(self):
         print("config", self.cpu, self.ram)
 
+    @classmethod
+    def get_manufacturer(self):
+        print(self.manufactur)
+
 comp1 = Computer('I5', '16gb')
 comp1.config()
+Computer.get_manufacturer()
 Computer.config(comp1)
